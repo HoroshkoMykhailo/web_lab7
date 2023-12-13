fedc4202488e9ebea5e3b2c9a786fc9438def403
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2023-12-13 19:35:41 +0200

    animation?

--- index.js
+++ index.js
@@ -11,18 +11,59 @@ function close(){
     const work = document.querySelector(".work");
     work.style.visibility = "hidden";
 }
-function start(){
+async function start(){
     const circle = document.querySelector(".circle");
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            elem.style.top = pos + "px"; 
-            elem.style.left = pos + "px"; 
-        }  
+    let pixels = 0;
+    let left = 0;
+    let top = 0;
+    while(true){
+        await moveleft(circle, left-pixels, left);
+        left = -pixels;
+        pixels++;
+        await movetop(circle, top-pixels, top);
+        top = -pixels;
+        pixels++;
+        await moveright(circle, left + pixels, left);
+        left = left + pixels;
+        pixels++;
+        await movebottom(circle, top + pixels, top);
+        top = top + pixels;
+        if(pixels > 100){
+            break;
+        }
+    }
+    
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function moveright(el, pixels, start){
+    while(start < pixels){
+        start++;
+        el.style.marginLeft = start + "px";
+        await sleep(1);
+    }
+}
+async function movebottom(el, pixels, start){
+    while(start < pixels){
+        start++;
+        el.style.marginTop = start + "px";
+        await sleep(1);
+    }
+}
+async function moveleft(el, pixels, start){
+    while(start > pixels){
+        start--;
+        el.style.marginLeft = start + "px";
+        await sleep(1);
+    }
+    
+}
+async function movetop(el, pixels, start){
+    while(start > pixels){
+        start--;
+        el.style.marginTop = start + "px";
+        await sleep(1);
     }
 }

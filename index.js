7d740691af2b47db91747058f3df6232dfb4da08
HoroshkoMykhailo <goroshkomihajlo@gmail.com>
2023-12-13 16:26:37 +0200

    animation_start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", async function () {
     document.getElementById("play").onclick = play;
     document.getElementById("close").onclick = close;
+    document.getElementById("start").onclick = start;
 });
 function play(){
     const work = document.querySelector(".work");
@@ -8,5 +9,20 @@ function play(){
 }
 function close(){
     const work = document.querySelector(".work");
-    work.style.visibility = "hide";
+    work.style.visibility = "hidden";
+}
+function start(){
+    const circle = document.querySelector(".circle");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++; 
+            elem.style.top = pos + "px"; 
+            elem.style.left = pos + "px"; 
+        }  
+    }
 }
